2fada7757029877955606325dac98b581ab5b6ac
Max Hurl <max@maxhurl.co.uk>
2015-12-21 14:15:23 +0000

    started adding more tests - is failing

---
+++ src/__tests__/components/BudgetListContainer.js
@@ -0,0 +1,24 @@
+/* global jest, describe, it, expect */
+jest.dontMock('../../components/BudgetListContainer');
+jest.dontMock('../../utils/objectMap');
+
+import TestUtils from 'react-addons-test-utils';
+import React from 'react-native'; // eslint-disable-line no-unused-vars
+
+// cannot use es6 modules syntax because
+// jest.dontMock & jest.autoMockOff()
+// do not understand ES6 modules yet
+const BudgetListContainer = require('../../components/BudgetListContainer').BudgetListContainer;
+
+describe('BudgetListContainer', function () {
+
+  const ReactNotNative = require('react'); // eslint-disable-line no-unused-vars
+
+  it('should render data correctly', function () {
+    var shallowRenderer = TestUtils.createRenderer();
+    shallowRenderer.render(<BudgetListContainer />);
+    var output = shallowRenderer.getRenderOutput();
+    expect(output).toEqual(true);
+  });
+
+});
