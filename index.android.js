965405750ccfe1ffc7a78250fe2887c905b2e3fe
Max Hurl <max@maxhurl.co.uk>
2016-05-13 18:08:02 +0100

    start adding pause/resume events

--- index.android.js
+++ index.android.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, AppState } from 'react-native';
 import { NavContainer } from './src/NavContainer';
+import PersistenceActions from './src/actions/PersistenceActions';
 
 class BudgetBlocks extends React.Component {
+
+  componentWillMount() {
+    this.handleAppStateChange('started');
+  }
+
+  componentDidMount() {
+    AppState.addEventListener('change', (appState) => this.handleAppStateChange(appState));
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', (appState) => this.handleAppStateChange(appState));
+  }
+
+  handleAppStateChange(appState) {
+    switch (appState) {
+      case 'started':
+        PersistenceActions.loadPersistentState();
+        break;
+      case 'inactive':
+        // unused
+        break;
+      case 'background':
+        PersistenceActions.persistState();
+        break;
+    }
+  }
 
   render() {
     return <NavContainer/>;
